1bdc23d126613e9cd8db3ff839d478415e97829c
yogi <yogi@aradhye.com>
2025-06-20 16:57:00 -0500

    added budget tracking

--- expenses.py
+++ expenses.py
@@ -3,12 +3,13 @@ from datetime import datetime
 class expense:
     def __init__(self):
         self.expenses = list()
+        self.budget = Decimal(0)
     def view(self):
         pass
     def add(self):
         expenseDate = self._getDate()
         expenseCategory = input("Enter category of the expense: ")
-        expenseAmount = self._getAmount()
+        expenseAmount = self._getAmount(inputmessage='Enter Expense amount: ')
         expenseDescription = input("Enter brief description of the expense: ")
         
         expense = dict(date= expenseDate,category = expenseCategory,  amount= expenseAmount ,  description= expenseDescription)
@@ -25,18 +26,33 @@ class expense:
             except ValueError:
                 print("Input valid date format")
 
-    def _getAmount(self):
+    def _getAmount(self, inputmessage='Enter amount: '):
         while True:
             try:
-                value = input("Enter amount: ")
+                value = input(inputmessage)
                 return Decimal(value)
             except InvalidOperation:
                 print("Input amount value")       
         
     def setBudget(self):
-        pass
+        self.budget = self._getAmount(inputmessage="Enter budget amount: ")
+    
+
     def trackBudget(self):
-        pass    
+        total = Decimal(0)    
+        for item in self.expenses:
+           total = total + item["amount"]
+
+        print(total)
+
+        if(total>self.budget):
+            print(f"You have exceeded the budget by {total - self.budget} amount. \n")
+        else:
+            print(f"You have {self.budget-total} left in your budget. \n")
+
+
+        #self.expenses(map(lambda T: T["amount"]))
+
     def saveExpenses(self):
         for item in self.expenses:
             print(item['date'],item['amount'])
@@ -54,19 +70,24 @@ def main():
         elif(option==2):
             e.view()
         elif(option==3):
-            e.trackBudget()
+            e.setBudget()
         elif(option==4):
-            e.saveExpenses()
+            e.trackBudget()
         elif(option==5):
+            e.saveExpenses()
+        elif(option==6):
+            break
+        else:
             break
 
 def showMenu():
     option = int(input("Select one of the options from below for the budget tracking: \n" \
     "1. Add Expense\n" \
     "2. View Expenses\n" \
-    "3. Track Budget\n" \
-    "4. Save Expenses\n" \
-    "5. Exit\n"))
+    "3. Set Budget\n" \
+    "4. Track Budget\n" \
+    "5. Save Expenses\n" \
+    "6. Exit\n"))
     
     return option
     
