445fff16f0df2589853e57c83e194bcc82f86a7d
yogi <yogi@aradhye.com>
2025-06-23 18:40:07 -0500

    minor clean up and code org

--- expenses.py
+++ expenses.py
@@ -17,22 +17,6 @@ class expense:
         self.expenses.append(expense)
 
         print("added \n")
-
-    def _getDate(self):
-        while True:
-            try:
-                value = input("Enter Date of the expense in the format YYYY-MM-DD: ")
-                return datetime.strptime(value, "%Y-%m-%d")
-            except ValueError:
-                print("Input valid date format")
-
-    def _getAmount(self, inputmessage='Enter amount: '):
-        while True:
-            try:
-                value = input(inputmessage)
-                return Decimal(value)
-            except InvalidOperation:
-                print("Input amount value")       
         
     def setBudget(self):
         self.budget = self._getAmount(inputmessage="Enter budget amount: ")
@@ -58,7 +42,23 @@ class expense:
             print(item['date'],item['amount'])
         print("saved \n")
        
-        
+    
+    def _getDate(self):
+        while True:
+            try:
+                value = input("Enter Date of the expense in the format YYYY-MM-DD: ")
+                return datetime.strptime(value, "%Y-%m-%d")
+            except ValueError:
+                print("Input valid date format")
+
+    def _getAmount(self, inputmessage='Enter amount: '):
+        while True:
+            try:
+                value = input(inputmessage)
+                return Decimal(value)
+            except InvalidOperation:
+                print("Input amount value")  
+             
         
 def main():
     e = expense()
@@ -81,7 +81,7 @@ def main():
             break
 
 def showMenu():
-    option = int(input("Select one of the options from below for the budget tracking: \n" \
+    option = int(input("\nSelect one of the options from below for the budget tracking: \n \n" \
     "1. Add Expense\n" \
     "2. View Expenses\n" \
     "3. Set Budget\n" \
